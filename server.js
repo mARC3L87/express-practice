e712bcea50f7e77871ff2cefa7867d3c7f2bdb71
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-10-03 17:23:51 +0200

    Add simple server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use('/user', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/views/forbidden.html'));
+});
+
+app.get('/', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/home', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/about', (req, res) =>{
+  res.show('about.html');
+});
+
+app.use(express.static(path.join(__dirname, '/views/image')));
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+});
+
+app.listen(8001, () => {
+  console.log('Server is running on port: 8001');
+});
+
